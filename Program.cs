beab224b68a8dd9637a617a64358c66f1ba2f8ad
Autumn <dazzleookami@outlook.com>
2025-12-19 01:26:36 -0800

    Implement Batch Export and automation functions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Text.Json;
 using AssetsTools.NET;
 using AssetsTools.NET.Extra;
@@ -40,8 +42,11 @@ namespace PatchmanUnity
             {
                 case "importasset":
                     return RunImportAsset(args) ? 0 : 2;
+
+                case "extractbundle":
+                    return RunExtractBundle(args) ? 0 : 3;
                 case "automation":
-                    return RunReadOps(args) ? 0 : 3;
+                    return RunReadOps(args) ? 0 : 4;
                 case "help":
                 case "-h":
                 case "--help":
@@ -61,6 +66,111 @@ namespace PatchmanUnity
             Console.WriteLine("  patcher.exe importintobundle --bundlePath=\"exampleBundlePath\" --assetName=\"exampleAssetName\" --exportPath=\"exampleExportPath\" --importPath=\"exampleImportPath\"");
         }
 
+        static bool RunExtractBundle(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.Error.WriteLine("extractbundle <bundlepath> <extractpath>");
+                return false;
+            }
+
+            if (!Directory.Exists(args[2]))
+            {
+                Console.WriteLine("Directory does not exist!");
+                return false;
+            }
+
+            HashSet<string> flags = GetFlags(args);
+
+            foreach (string file in Directory.EnumerateFiles(args[2]))
+            {
+                string decompFile = $"{file}.decomp";
+
+                if (flags.Contains("-md"))
+                    decompFile = null;
+
+                if (!File.Exists(file))
+                {
+                    Console.WriteLine($"File {file} does not exist!");
+                    return false;
+                }
+
+                DetectedFileType fileType = FileTypeDetector.DetectFileType(file);
+                if (fileType != DetectedFileType.BundleFile)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Decompressing {file}...");
+                AssetBundleFile bun = DecompressBundle(file, decompFile);
+
+                int entryCount = bun.BlockAndDirInfo.DirectoryInfos.Count;
+                for (int i = 0; i < entryCount; i++)
+                {
+                    string name = bun.BlockAndDirInfo.DirectoryInfos[i].Name;
+                    byte[] data = BundleHelper.LoadAssetDataFromBundle(bun, i);
+                    string outName;
+                    if (flags.Contains("-keepnames"))
+                        outName = Path.Combine(args[2], name);
+                    else
+                        outName = Path.Combine(args[2], $"{Path.GetFileName(file)}_{name}");
+                    Console.WriteLine($"Exporting {outName}...");
+                    File.WriteAllBytes(outName, data);
+                }
+
+                bun.Close();
+
+                if (!flags.Contains("-kd") && !flags.Contains("-md") && File.Exists(decompFile))
+                    File.Delete(decompFile);
+
+                Console.WriteLine("Done.");
+            }
+
+            return true;
+        }
+
+        static HashSet<string> GetFlags(string[] args)
+        {
+            HashSet<string> flags = new HashSet<string>();
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("-"))
+                    flags.Add(args[i]);
+            }
+            return flags;
+        }
+
+        static AssetBundleFile DecompressBundle(string file, string? decompFile)
+        {
+            AssetBundleFile bun = new AssetBundleFile();
+
+            Stream fs = File.OpenRead(file);
+            AssetsFileReader r = new AssetsFileReader(fs);
+
+            bun.Read(r);
+            if (bun.Header.GetCompressionType() != 0)
+            {
+                Stream nfs;
+                if (decompFile == null)
+                    nfs = new MemoryStream();
+                else
+                    nfs = File.Open(decompFile, FileMode.Create, FileAccess.ReadWrite);
+
+                AssetsFileWriter w = new AssetsFileWriter(nfs);
+                bun.Unpack(w);
+
+                nfs.Position = 0;
+                fs.Close();
+
+                fs = nfs;
+                r = new AssetsFileReader(fs);
+
+                bun = new AssetBundleFile();
+                bun.Read(r);
+            }
+
+            return bun;
+        }
         static bool RunReadOps(string[] args)
         {
             if (args.Length < 2)
@@ -108,7 +218,7 @@ namespace PatchmanUnity
 
             RunImportAssetBatch(ops);
 
-            if (changed) 
+            if (changed)
             {
                 using AssetsFileWriter writer = new(ops.saveFilePath);
                 afile.Write(writer);                        
@@ -222,5 +332,72 @@ namespace PatchmanUnity
             }
             return true;
         }
+
+        
+    }
+}
+
+namespace PatchmanUnity
+{
+    public static class FileTypeDetector
+    {
+        public static DetectedFileType DetectFileType(string filePath)
+        {
+            using (FileStream fs = File.OpenRead(filePath))
+            using (AssetsFileReader r = new AssetsFileReader(fs))
+            {
+                return DetectFileType(r, 0);
+            }
+        }
+
+        public static DetectedFileType DetectFileType(AssetsFileReader r, long startAddress)
+        {
+            string possibleBundleHeader;
+            int possibleFormat;
+            string emptyVersion, fullVersion;
+
+            r.BigEndian = true;
+
+            if (r.BaseStream.Length < 0x20)
+            {
+                return DetectedFileType.Unknown;
+            }
+            r.Position = startAddress;
+            possibleBundleHeader = r.ReadStringLength(7);
+            r.Position = startAddress + 0x08;
+            possibleFormat = r.ReadInt32();
+
+            r.Position = startAddress + (possibleFormat >= 0x16 ? 0x30 : 0x14);
+
+            string possibleVersion = "";
+            char curChar;
+            while (r.Position < r.BaseStream.Length && (curChar = (char)r.ReadByte()) != 0x00)
+            {
+                possibleVersion += curChar;
+                if (possibleVersion.Length > 0xFF)
+                {
+                    break;
+                }
+            }
+            emptyVersion = Regex.Replace(possibleVersion, "[a-zA-Z0-9\\.\\n\\-]", "");
+            fullVersion = Regex.Replace(possibleVersion, "[^a-zA-Z0-9\\.\\n\\-]", "");
+
+            if (possibleBundleHeader == "UnityFS")
+            {
+                return DetectedFileType.BundleFile;
+            }
+            else if (possibleFormat < 0xFF && emptyVersion.Length == 0 && fullVersion.Length >= 5)
+            {
+                return DetectedFileType.AssetsFile;
+            }
+            return DetectedFileType.Unknown;
+        }
+    }
+
+    public enum DetectedFileType
+    {
+        Unknown,
+        AssetsFile,
+        BundleFile
     }
 }
